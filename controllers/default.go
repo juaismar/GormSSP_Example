e85f94fc5dccb3072bf4ac99a3b0d99711da93dd
Juan Iscar <juan.iscar@exponentiateam.com>
2021-03-17 12:34:12 +0100

    Signed-off-by: Juan Iscar <juan.iscar@exponentiateam.com>

--- controllers/default.go
+++ controllers/default.go
@@ -20,11 +20,12 @@ func (c *MainController) SimplePaginate() {
 
 	var err error
 
-	columns := make(map[int]SSP.Data)
-	columns[0] = SSP.Data{Db: "id", Dt: "id", Formatter: nil}
-	columns[1] = SSP.Data{Db: "name", Dt: "name", Formatter: nil}
-	columns[2] = SSP.Data{Db: "email", Dt: "email", Formatter: nil}
-	columns[3] = SSP.Data{Db: "age", Dt: "age", Formatter: nil}
+	columns := []SSP.Data{
+		SSP.Data{Db: "id", Dt: "id", Formatter: nil},
+		SSP.Data{Db: "name", Dt: "name", Formatter: nil},
+		SSP.Data{Db: "email", Dt: "email", Formatter: nil},
+		SSP.Data{Db: "age", Dt: "age", Formatter: nil},
+	}
 
 	c.Data["json"], err = SSP.Simple(c, models.Conn, "users", columns)
 	if err != nil {
